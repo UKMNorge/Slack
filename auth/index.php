f70858c9988937767fdf551428996cce5a5a678a
Marius Mandal <mariusmandal@gmail.com>
2019-06-02 01:25:40 +0200

    Flyttet lagring av token til app

--- auth/index.php
+++ auth/index.php
@@ -3,43 +3,21 @@
 namespace UKMNorge\Slack\Kjop;
 
 use UKMNorge\Slack\API\OAuth;
-use SQLins;
-
+use UKMNorge\Slack\Kjop\APP;
 use Exception;
 
 require_once('../config.inc.php');
 require_once('UKM/sql.class.php');
 
+// IF WE GOT A CODE, TRY AUTH
 if( isset( $_GET['code'] ) ) {
 	try {
 		$approval = OAuth::access( $_GET['code'] );
-
-		$sql = new SQLins('slack_access_token');
-		$sql->add('team_id', $approval->team_id);
-		$sql->add('team_name', $approval->team_name);
-		$sql->add('access_token', $approval->access_token);
-		$sql->add('data', json_encode( $approval ));
-		
-		$res = $sql->run();
-		if( !$res ) {
-			throw new Exception('Kunne ikke lagre data.');
-		} else {
-			echo 'Suksess!';
-			die();
-		}
+		APP::storeAPIAccessToken($approval);
 	} catch( Exception $e ) {
 		echo '<h1>Beklager!</h1>'. $e->getMessage();
 	}
 }
 
-
-echo '<p>'.
-	'<a href="'.
-	'https://slack.com/oauth/authorize?scope=incoming-webhook,commands'.
-	'&client_id='. SLACK_CLIENT_ID .
-	'&redirect_uri='. OAuth::getRedirectUrl() .
-	'">'.
-	'<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" '.
-		' srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />'.
-	'</a>'.
-	'</p>';
+// IF ANYTHING BUT SUCCESS, SHOW BUTTON
+echo '<p>'. Oauth::getButton() .'</p>';
--- kjop/class/Kjop.php
+++ kjop/class/Kjop.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace UKMNorge\Slack\Kjop;
+
 use UKMNorge\Slack\Response;
 use \Exception;
 
 class APP
 {
+	const TABLE = 'slack_access_token';
+
 	// Die if one or more constants is missing
 	public static function init()
 	{
@@ -18,7 +21,7 @@ class APP
 					'SLACK_CLIENT_ID'
 				]
 			);
-		} catch( Exception $e ) {
+		} catch (Exception $e) {
 			$response = new Response(
 				'ephemeral',
 				':sob: Beklager, systemet er feil satt opp. Kontakt support@ukm.no'
@@ -27,14 +30,54 @@ class APP
 		}
 	}
 
+	public static function setAPITokenFromTeamId( int $team_id)
+	{
+		$sql = new SQL("SELECT `access_token`
+			FROM `#table`
+			WHERE `team_id` = '#team'",
+			[
+				'table' => self::TABLE,
+				'team' => $team_id
+			]
+			);
+		$res = $sql->run('field', 'access_token');
+
+		if( !$res ) {
+			$response = new Response(
+				'ephemeral',
+				':sob: Beklager, kan ikke se at teamet ditt er godkjent for bruk av denne appen. Kontakt support@ukm.no'
+			);
+			$response->renderAndDie();
+		}
+		
+		define('SLACK_ACCESS_TOKEN', $res);
+
+		return true;
+	}
+
+	public static function storeAPIAccessToken($data)
+	{
+		$sql = new SQLins(self::TABLE);
+		$sql->add('team_id', $data->team_id);
+		$sql->add('team_name', $data->team_name);
+		$sql->add('access_token', $data->access_token);
+		$sql->add('data', json_encode($data));
+
+		$res = $sql->run();
+		 if(!$res) {
+			throw new Exception('Kunne ikke lagre data.');
+		}
+		return true;
+	}
+
 	public static function validateConstants($constants)
 	{
 
 		foreach ($constants as $constant) {
 			if (!defined($constant)) {
-				error_log('SETUP: Missing constant '. $constant);
+				error_log('SETUP: Missing constant  '. $constant);
 				throw new Exception(
-					'Missing constant '. $constant,
+					'Missing constant  '. $constant,
 					181002
 				);
 			}
